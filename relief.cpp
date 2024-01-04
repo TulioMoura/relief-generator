1fe9d077038bd21e31afbc0e94757672fc697e7c
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-01-04 01:46:45 +0000

    changed logic to generate relief

--- relief.cpp
+++ relief.cpp
@@ -11,13 +11,19 @@
     srand(x*y);
      float c =  (rand()%100);
      c=c/100;
+    float return_value = (a/2 + b/2 + c/2 - ((a*b)/5 + (b*c)/5));
+    if(return_value<=0){
+        printf("%f\n",return_value);
+    }
+    return return_value;
 
-    return (a/4 + b/4 + c/2);
 }
 
 int main(){
     std::ofstream file;
+    std::ofstream log_file;
     file.open("img.ppm");
+    log_file.open("log.txt");
     file<<"P3 200 200 255\n";
     for(int i =0; i< 200; i++ ){
         for( int j = 0; j< 200; j++){
@@ -34,9 +40,11 @@ int main(){
             }
 
             file<<red<<" "<<green<<" "<<0<<std::endl;
+            log_file<<(terrain_heigth/512)<<std::endl;
         }
         //printf("%i\n",i);
     }
     file.close();
+    log_file.close();
     return 0;
 }
