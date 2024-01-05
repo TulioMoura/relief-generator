094ad663b028191a4643ef88eb79d7a8ba29b44c
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-01-05 00:06:41 +0000

    upgraded generation logic

--- relief.cpp
+++ relief.cpp
@@ -1,19 +1,24 @@
 #include<stdio.h>
 #include<fstream>
 #include<math.h>
- float reliefGenerator(int x, int y){
-    srand(x*10);
+ float reliefGenerator(int x, int y, int seed){
+    srand(x*seed);
+    float reference = cos((x*y)/10) * sin((y-x)/10);
+    printf("%f ref \n" , reference);
      float a = (rand()%100);
      a=a/100;
-    srand(y*10);
+     a = a +reference ;
+    srand(y*seed/2);
      float b = (rand()%100);
      b=b/100;
-    srand(x*y);
+     b=b +reference ;
+    srand(x*y*(seed/4));
      float c =  (rand()%100);
      c=c/100;
-    float return_value = (a/2 + b/2 + c/2 - ((a*b)/5 + (b*c)/5));
+     c=c + reference;
+    float return_value = abs(a/2 + b/2 + c/2 );
     if(return_value<=0){
-        printf("%f\n",return_value);
+        printf("%f || %i # %i\n",return_value,x,y);
     }
     return return_value;
 
@@ -23,11 +28,11 @@ int main(){
     std::ofstream file;
     std::ofstream log_file;
     file.open("img.ppm");
-    log_file.open("log.txt");
-    file<<"P3 200 200 255\n";
-    for(int i =0; i< 200; i++ ){
-        for( int j = 0; j< 200; j++){
-            int terrain_heigth = reliefGenerator(i, j) *512;
+    //log_file.open("log.txt");
+    file<<"P3 100 100 255\n";
+    for(int i =0; i< 100; i++ ){
+        for( int j = 0; j< 100; j++){
+            int terrain_heigth = reliefGenerator(i, j,252525) *512;
             int red;
             int green;
             if(terrain_heigth > 256){
@@ -40,11 +45,11 @@ int main(){
             }
 
             file<<red<<" "<<green<<" "<<0<<std::endl;
-            log_file<<(terrain_heigth/512)<<std::endl;
+            //log_file<<(terrain_heigth/512)<<std::endl;
         }
         //printf("%i\n",i);
     }
     file.close();
-    log_file.close();
+    //log_file.close();
     return 0;
 }
