725feab9389c2379a27f35608d638e4fc5cfb75e
Túlio <90994955+TulioMoura@users.noreply.github.com>
2024-01-07 02:35:27 +0000

    started the perlin noise implementation

--- relief.cpp
+++ relief.cpp
@@ -3,8 +3,9 @@
 #include<math.h>
  float reliefGenerator(int x, int y, int seed){
     srand(x*seed);
-    float reference = cos((x*y)/10) * sin((y-x)/10);
-    printf("%f ref \n" , reference);
+    float temp = cos( ((x*y)/5) * ((y*x)/2));
+    float reference = temp;
+    printf(" %f  * %f ref \n" ,temp, reference);
      float a = (rand()%100);
      a=a/100;
      a = a +reference ;
@@ -16,11 +17,52 @@
      float c =  (rand()%100);
      c=c/100;
      c=c + reference;
-    float return_value = abs(a/2 + b/2 + c/2 );
+    float return_value = abs(a/4 + b/4 + c/4 );
     if(return_value<=0){
         printf("%f || %i # %i\n",return_value,x,y);
     }
     return return_value;
+
+}
+
+struct vec2{
+    float x, y;
+};
+
+vec2 randomVector(int vx, int vy, int seed){
+    srand(seed+vx);
+    float x = rand()%1000;
+    srand(seed+vy);
+    float y = rand()%1000;
+    x = cos(x);
+    y = cos(y);
+    vec2 v;
+    v.x = x;
+    v.y = y;
+    return v;
+}
+
+float dotProduct(int x, int y, float px, float py, int seed){
+    vec2 v = randomVector(x, y,seed);
+
+    float distancex = px-x;
+    float distancey = py-y;
+
+    float dotProductx = distancex * v.x;
+    float dotProducty = distancey * v.y;
+
+    return (dotProductx+dotProducty);
+    }
+
+float perlin(float x, float y, int seed){
+
+    int x0 = floor(x);
+    int y0 = floor(y);
+   
+    float n = dotProduct(x0,y0,x,y,seed);
+
+    return n;
+
 
 }
 
@@ -32,7 +74,7 @@ int main(){
     file<<"P3 100 100 255\n";
     for(int i =0; i< 100; i++ ){
         for( int j = 0; j< 100; j++){
-            int terrain_heigth = reliefGenerator(i, j,252525) *512;
+            int terrain_heigth = ((perlin(i, j,252525)*0.5)+0.5) *512;
             int red;
             int green;
             if(terrain_heigth > 256){
